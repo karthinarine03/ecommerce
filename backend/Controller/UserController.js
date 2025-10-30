c13e4f188228b742ac024afbb01d341358703fe5
thiruvenkatasamyiyya <thiruvenkatasamyiyya@gmail.com>
2025-10-30 05:45:24 +0530

    add middlewares

--- backend/Controller/UserController.js
+++ backend/Controller/UserController.js
@@ -4,3 +4,15 @@ import catchAsynError from "../Middlewares/catchAsynError.js";
 export const getUser=catchAsynError(async(req,res)=>{
         res.send("hi aaya");
 })
+
+// regiser user
+
+
+// login user
+
+
+//forget password
+
+
+
+//reset password
