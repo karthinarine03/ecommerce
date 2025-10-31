4a3fe8b52bc9224620f628dba0b1e10d2722b55c
karthinarine03 <karthinarine03@gmail.com>
2025-10-31 21:24:00 +0530

    Added files

--- backend/Controller/UserController.js
+++ backend/Controller/UserController.js
@@ -1,18 +1,29 @@
 import catchAsynError from "../Middlewares/catchAsynError.js";
+import User from "../Model/User.js";
+import sendtoken from "../Utils/sendtoken.js";
 
 
-export const getUser=catchAsynError(async(req,res)=>{
-        res.send("hi aaya");
+export const register=catchAsynError(async(req,res)=>{
+        const {name,email,password}=req.body;
+        const user=await User.create({name,email,password});
+        
+        sendtoken(user,201,res)
+});
+
+export const login=catchAsynError(async(req,res)=>{
+        const {email,password}=req.body;
+        const user=await User.findOne({email}).select("+password");
+        const ispasswordmatched=await user.comparepasword(password);
+        sendtoken(user,201,res);
 })
 
-// regiser user
+export const logout=catchAsynError(async(req,res)=>{
 
-
-// login user
-
-
-//forget password
-
-
-
-//reset password
+        res.cookie("token",null,{
+                expires:new Date(Date()),
+                httpOnly:true
+        })
+        res.status(201).json({
+                message:"logged out"
+        })
+})
